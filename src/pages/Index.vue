a8d6e4945e741da8a79ac6142460dd6c3ac3a5a8
DDN <digitaldiyninja@gmail.com>
2019-10-03 21:21:31 +0100

    no alt

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,53 +1,19 @@
 <template>
-  <Layout>
-    <div class="container">
-      <Hero />
-      <Nav />
-
-      <ProjectsGrid :projects="$page.projects.edges" />
-    </div>
-    <LatestJournals :journals="$page.journals.edges" />
-  </Layout>
+<Nav />
 </template>
 
-<page-query>
-query Posts {
-	projects: allProjectPost {
-    edges {
-      node {
-        id
-        date (format: "YYYY")
-        title
-        categories
-        thumbnail (quality: 90)
-        path
-      }
-    }
-  },
-  journals: allJournalPost (perPage: 4) {
-    edges {
-      node {
-        id
-        path
-        title
-      }
-    }
-  }
-}
-</page-query>
+
 
 <script>
-import Hero from "@/components/Hero"
+
 import Nav from "@/components/Nav"
-import ProjectsGrid from "@/components/ProjectsGrid"
-import LatestJournals from "@/components/LatestJournals"
+
 
 export default {
   components: {
-    Hero,
+  
     Nav,
-    ProjectsGrid,
-    LatestJournals
+
   }
 }
 </script>
