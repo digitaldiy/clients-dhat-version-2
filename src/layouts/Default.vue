10f54c895a3dc6c7e3215905fdfb65c257c1ff49
DDN <digitaldiyninja@gmail.com>
2019-10-03 20:05:00 +0100

    nav test

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -8,11 +8,12 @@
 
 <script>
 import Header from "@/components/Header"
+import Nav from "@/components/Nav"
 import Footer from "@/components/Footer"
 
 export default {
   components: {
-    Header,
+    Nav,
     Footer
   }
 }
