1203889b54d2158a8e24abe65f0ad8d747b8184a
digitaldiy <36301244+digitaldiy@users.noreply.github.com>
2019-10-04 13:35:26 +0100

    full fix

--- src/components/Full.vue
+++ src/components/Full.vue
@@ -6,13 +6,16 @@
     <span class="md-body-1">Body 1</span>
     <span class="md-caption">Caption</span>
     </div>
+     <div class="section">
+       
+    <span class="md-caption">Caption</span>
+    </div>
   </full-page>
 </div>
 
 
 <script>
-import 'fullpage.js/vendors/scrolloverflow' // Optional. When using scrollOverflow:true
-import './fullpage.scrollHorizontally.min' // Optional. When using fullpage extensions
+
 import VueFullPage from 'vue-fullpage.js'
 
 Vue.use(VueFullPage);
