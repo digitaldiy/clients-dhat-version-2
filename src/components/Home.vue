49b9f6716be34ebee46f0af22f461ce03086d5e6
digitaldiy <36301244+digitaldiy@users.noreply.github.com>
2019-10-04 12:29:20 +0100

    fix

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,5 @@
 <template>
 <div>
-  <b-jumbotron header="BootstrapVue" lead="Bootstrap v4 Components for Vue.js 2">
-    <p>For more information visit website</p>
-    <b-button variant="primary" href="#">More Info</b-button>
-  </b-jumbotron>
+   <span class="md-display-4">Display 4</span>
 </div>
 </template>
